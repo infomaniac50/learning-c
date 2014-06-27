59b0acb6bfb3115acc5211a4f93b1571e738d31a
Derek Chafin <infomaniac50@gmail.com>
2014-06-27 11:36:57 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Learning C ##
+
+A repository to brush up on my C skills.
+
+### License ###
---
+++ motors/power.c
@@ -0,0 +1,15 @@
+#include "math.h"
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+  /*
+  Torque in Inch Pounds-force = Horsepower * 63025 / Motor RPM
+  INPUTS: Horsepower, Motor RPM
+  OUTPUTS: Torque in Inch Pounds-force
+   */
+
+  float horsepower, motor_rpm = 0.0;
+
+  return 0;
+}
