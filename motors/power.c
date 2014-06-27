6212d2b12891622ba8ba093356dc9796f6e7c233
Derek Chafin <infomaniac50@gmail.com>
2014-06-27 13:21:18 -0500

    Added motor power conversion.

--- motors/power.c
+++ motors/power.c
@@ -1,6 +1,9 @@
 #include "math.h"
 #include "stdio.h"
 
+
+void usage();
+
 int main(int argc, char const *argv[])
 {
   /*
@@ -8,8 +11,33 @@ int main(int argc, char const *argv[])
   INPUTS: Horsepower, Motor RPM
   OUTPUTS: Torque in Inch Pounds-force
    */
-
   float horsepower, motor_rpm = 0.0;
+  float torque;
+
+  if (argc < 3){
+    usage();
+    return 1;
+  }
+
+  // printf("%s\n", argv[1]);
+  // printf("%s\n", argv[2]);
+  // return 0;
+
+  if (sscanf(argv[1], "%f", &horsepower) != 1)
+    usage();
+
+  if (sscanf(argv[2], "%f", &motor_rpm) != 1)
+    usage();
+
+  torque = horsepower * 63025 / motor_rpm;
+  printf("%s\n", "Torque in inch pounds-force");
+  printf("%.5f\n", torque);
 
   return 0;
+}
+
+void usage() {
+  printf("%s\n", "Power - Derek Chafin");
+  printf("%s\n", "power <horsepower> <motor rpm>");
+  printf("%s\n", "Calculates torque in inch pounds-force.");
 }
